73e90c518e5f16214eaf3dd7f9322f899ffc780d
Parker Jones <elder.jones888@gmail.com>
2023-01-20 16:36:35 -0700

    Week 3 Team

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,14 @@
 import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import { getLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+
+const productId = getParam("product");
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
 function addProductToCart(product) {
   const products = getLocalStorage("so-cart") || [];
