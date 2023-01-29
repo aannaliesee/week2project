f5487e13353f37b96396aba163567ba08f2b8b37
Parker Jones <elder.jones888@gmail.com>
2023-01-28 17:35:10 -0700

    ability to add multiple items!

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -37,7 +37,7 @@ export default class ProductDetails {
       .addEventListener("click", this.addToCart.bind(this));
   }
   addToCart() {
-    const products = getLocalStorage && [];
+    const products = getLocalStorage("so-cart") || [];
     products.push(this.product);
     setLocalStorage("so-cart", products);
   }
