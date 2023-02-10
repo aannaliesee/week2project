b852c8ab51f14f0fbd8c0b70c94ed2ef614fe887
a-chavez474 <abelinkpact@gmail.com>
2023-02-10 15:33:13 -0700

    First try

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,9 @@
-import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.mjs";
+
 import { loadHeaderFooter } from "./utils.mjs";
 
-const dataSource = new ProductData("tents");
-const element = document.querySelector(".product-list");
-const list = new ProductList("Tents", dataSource, element);
+
 
 loadHeaderFooter();
 
-list.init();
+
 
