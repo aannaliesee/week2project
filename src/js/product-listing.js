7f654a52afb0f7d04a0f27a63813cbbc97884304
a-chavez474 <abelinkpact@gmail.com>
2023-02-10 16:21:02 -0700

    Second try

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -3,12 +3,13 @@ import { loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
+loadHeaderFooter();
 
 const dataSource = new ProductData("tents");
 const element = document.querySelector(".product-list");
 const list = new ProductList("Tents", dataSource, element);
 
-loadHeaderFooter();
+
 
 list.init();
 
