6fae44959e9b09a1e9cba63b47486f08a23c9410
aannaliesee <71413793+aannaliesee@users.noreply.github.com>
2023-02-10 22:54:50 -0700

    Step 2 and 3

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 
 import { loadHeaderFooter } from "./utils.mjs";
 
-
-
 loadHeaderFooter();
+
+
 
 
 
--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,13 +1,14 @@
-import { loadHeaderFooter } from "./utils.mjs";
+import { loadHeaderFooter, getParam } from "./utils.mjs";
 
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
 loadHeaderFooter();
 
-const dataSource = new ProductData("tents");
+const category = getParam("category");
+const dataSource = new ProductData();
 const element = document.querySelector(".product-list");
-const list = new ProductList("Tents", dataSource, element);
+const list = new ProductList(category, dataSource, element);
 
 
 
