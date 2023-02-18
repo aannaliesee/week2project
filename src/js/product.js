c2db90351101dd919a961f60e27b9792a6424cf1
a-chavez474 <abelinkpact@gmail.com>
2023-02-17 21:05:43 -0700

    Step 5 done, please review.

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,10 @@
-import ProductData from "./ProductData.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 import { getParam } from "./utils.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 import { amountInCart } from "./utils.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
-const dataSource = new ProductData("tents");
+const dataSource = new ExternalServices("tents");
 
 const productId = getParam("product");
 const product = new ProductDetails(productId, dataSource);
