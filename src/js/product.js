d36bb50244694f0b9739e146a146d6b2f0c18224
aannaliesee <71413793+aannaliesee@users.noreply.github.com>
2023-03-02 23:30:06 -0700

    AM-ProductComment

--- src/js/product.js
+++ src/js/product.js
@@ -34,3 +34,70 @@ async function addToCartHandler(e) {
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);*/
+
+
+//==========================================//
+  //Added by AM for product comment//
+  //get comment from form with id 
+const commentForm = document.querySelector("comment-form");
+
+//add an event listener to comment form 
+commentForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    // execute this function when the button is clicked
+
+    //get cpmment test from form input
+
+const commentTextInput = document.getElementById("#comment-text");
+const commentText = commentTextInput.value.trim();
+
+//create javascriipt object to represent comment 
+const newComment = {
+    text: commentText
+};
+
+// Check if there is any existing comment data stored in local storage
+//const productId = ${productId};
+  let comments = JSON.parse(localStorage.getItem(`product-${productId}-comments`));
+  if (!comments) {
+    comments = [];
+  }
+
+//add new comment object to array
+comments.push(newComment);
+
+//save updated array of comments to local storage
+localStorage.setItem(`product-${productId}-comments`, JSON.stringify(comments));
+
+  // Clear the comment form inputs ???
+  commentTextInput.value = "";
+
+displayComments();
+});
+
+//display comments on page 
+
+function displayComments() {
+    const commentsContainer = document.querySelector("#comments-container");
+
+    //retrieve comment from local storage
+
+    let comments = JSON.parse(localStorage.getItem(`product-${productId}-comments`));
+    if (!comments){
+        comments = [];
+    }
+
+    //html for each comment and ad it to comments
+    let commentsHTML = "";
+    for (const comment of comments){
+        commentsHTML += `
+        <div class="comment">
+        <p>${comment.text}</p>
+        </div>
+        `;
+    }
+    commentsContainer.innerHTML = commentsHTML;
+}
+
+displayComments();
